d18bdb3cf90397295d73b684c6a71762926d7c18
Jamie Morrison <jamie@jamiemorrison.com>
2022-11-23 18:08:55 +0800

    Create Cluster ECR Repo

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
   value       = module.eks_blueprints.configure_kubectl
 }
+
+output "cluster_repo" {
+  description = "Cluster ECR URL to upload images."
+  value       = aws_ecr_repository.cluster_repo.repository_url
+}
