4be27a359d9cf9ee982174aab184174564e49347
Jamie Morrison <jamie@jamiemorrison.com>
2022-11-25 13:46:12 +0800

    CronJobs to scale nginx-demo minReplicas by time

---
+++ cronjobs.tf
@@ -0,0 +1,63 @@
+#---------------------------------------------------------------
+# CronJobs to scale minReplicas up and down
+#---------------------------------------------------------------
+
+resource "kubernetes_cron_job" "nginx_scale_up" {
+  metadata {
+    name = "nginx-scale-up"
+  }
+  spec {
+    concurrency_policy            = "Replace"
+    failed_jobs_history_limit     = 5
+    schedule                      = "0,30 * * * *"
+    starting_deadline_seconds     = 10
+    successful_jobs_history_limit = 10
+    job_template {
+      metadata {}
+      spec {
+        backoff_limit              = 2
+        ttl_seconds_after_finished = 10
+        template {
+          metadata {}
+          spec {
+            container {
+              name    = "kubectl"
+              image   = aws_ecr_repository.cluster_repo.name
+              command = ["/bin/sh", "-c", "kubectl patch hpa ${kubernetes_horizontal_pod_autoscaler.nginx_demo.metadata[0].name} -p '{\"spec\":{\"minReplicas\": 10}}'"]
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_cron_job" "nginx_scale_down" {
+  metadata {
+    name = "nginx-scale-down"
+  }
+  spec {
+    concurrency_policy            = "Replace"
+    failed_jobs_history_limit     = 5
+    schedule                      = "15,45 * * * *"
+    starting_deadline_seconds     = 10
+    successful_jobs_history_limit = 10
+    job_template {
+      metadata {}
+      spec {
+        backoff_limit              = 2
+        ttl_seconds_after_finished = 10
+        template {
+          metadata {}
+          spec {
+            container {
+              name    = "kubectl"
+              image   = aws_ecr_repository.cluster_repo.name
+              command = ["/bin/sh", "-c", "kubectl patch hpa ${kubernetes_horizontal_pod_autoscaler.nginx_demo.metadata[0].name} -p '{\"spec\":{\"minReplicas\": 2}}'"]
+            }
+          }
+        }
+      }
+    }
+  }
+}
